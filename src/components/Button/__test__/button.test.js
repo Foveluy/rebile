1246d2fb540c9b2515f0c89a71518d83dad417cc
方正 <215566435@qq.com>
2018-06-18 23:08:26 +0800

    feat: ci&cov

---
+++ src/components/Button/__test__/button.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import Button from '..';
+
+configure({ adapter: new Adapter() });
+
+describe('Button component test', () => {
+  it('button `style` testing ', () => {
+    const wrapper = mount(<Button />);
+    const cov = wrapper.find('.rb-button');
+    const style = cov.getDOMNode().style;
+  });
+});
