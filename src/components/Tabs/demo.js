6e9978e60160530f24c9165df0ef171eaeafdbb7
方正 <215566435@qq.com>
2018-06-21 18:46:01 +0800

    chore: adding demos

--- src/components/Tabs/demo.js
+++ src/components/Tabs/demo.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import Tab from '.';
+import WhiteSpace from '../Blank';
 
-const Tabs = [{ title: 'First' }, { title: 'Second' }, { title: 'Third' }, { title: 'Forth' }];
+const Tabs = [{ title: 'First' }, { title: 'Second' }, { title: 'Third' }];
 
-const TabsDemo = () => (
+const Normal = () => (
   <Tab tabs={Tabs}>
     <div style={{ height: 100, backgroundColor: 'white' }}>tab1</div>
     <div style={{ height: 100, backgroundColor: 'white' }}>tab2</div>
     <div style={{ height: 100, backgroundColor: 'white' }}>tab3</div>
-    <div style={{ height: 100, backgroundColor: 'white' }}>tab4</div>
   </Tab>
+);
+
+const TintColor = () => (
+  <Tab tabs={Tabs} tintColor="green">
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab1</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab2</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab3</div>
+  </Tab>
+);
+
+const Tabs5 = [{ title: 'First' }, { title: 'Second' }, { title: 'Third' }, { title: 'Forth' }, { title: 'Fifth' }];
+
+const Over3Tabs = () => (
+  <Tab tabs={Tabs5}>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab1</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab2</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab3</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab4</div>
+    <div style={{ height: 100, backgroundColor: 'white' }}>tab5</div>
+  </Tab>
+);
+
+const TabsDemo = () => (
+  <div>
+    <Normal />
+    <WhiteSpace />
+    <TintColor />
+    <WhiteSpace />
+    <Over3Tabs />
+  </div>
 );
 
 export default TabsDemo;
