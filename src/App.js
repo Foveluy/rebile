7901142932ef391336092340ced878ebf2151473
方正 <215566435@qq.com>
2018-06-21 09:58:10 +0800

    chore: adding reach/router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import ButtonDemos from './components/Button/demo';
 import TabsDemo from './components/Tabs/demo';
+import { Router } from '@reach/router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TabsDemo />
-        <ButtonDemos />
+        <Router>
+          <TabsDemo path="/" />
+          <ButtonDemos path="buttons" />
+        </Router>
       </div>
     );
   }
