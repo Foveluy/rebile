76b0594cb6e38c63cdc4b981e2b2d25767590ddb
方正 <215566435@qq.com>
2018-06-23 23:25:13 +0800

    chore: mdx kicking in

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -198,7 +198,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs|md)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import ButtonDemos from './components/Button/demo';
 import TabsDemo from './components/Tabs/demo';
 import { Router } from '@reach/router';
+import DemoMD from './components/Tabs/demo.md';
 
 const Home = () => (
   <div>
+    <DemoMD />
     <iframe width="375" height="600" src="http://localhost:3000/tabs" frameBorder="0" />
   </div>
 );
