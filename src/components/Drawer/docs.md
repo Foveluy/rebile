7de1f628998d690f377f401cbddc07601d0ac1d2
方正 <215566435@qq.com>
2018-07-02 23:56:21 +0800

    chore: setup drawer

--- src/components/Drawer/docs.md
+++ src/components/Drawer/docs.md
@@ -1,5 +1,5 @@
-# 
-
+# Drawer
+<br/>  Drawer 组件，使用与侧边栏导航，选择<br/>  底层由 `react-motion` 提供<br/> 
 
 
 ## API 描述
---
+++ src/components/Drawer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Drawer 组件，使用与侧边栏导航，选择
+ * 底层由 `react-motion` 提供
+ */
+class Drawer extends React.Component {
+  render() {
+    return <div className="rb-drawer" />;
+  }
+}
+
+export default Drawer;
